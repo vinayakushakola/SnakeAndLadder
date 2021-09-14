4406b01e72dbdbaa5dc532576b32a61751f55967
vinayak ushakola <vinayak.ushakola@gmail.com>
2021-09-14 21:50:46 +0530

    Added Welcome Message

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("================ Welcome To Snake And Ladder Game ================");
+            Console.ReadKey();
+        }
+    }
+}
