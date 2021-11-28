3229f7a5169410e7d73508cb0a88133aca6acb54
vinayak ushakola <vinayak.ushakola@gmail.com>
2021-11-28 10:12:20 +0530

    Added: Rolled a die

--- SnakeAndLadder/clsSnakeAndLadder.cs
+++ SnakeAndLadder/clsSnakeAndLadder.cs
@@ -6,9 +6,25 @@ namespace SnakeAndLadder
 {
     class clsSnakeAndLadder
     {
-        private int PlayerPosition;
+        private int PlayerPosition; // Default Value = 0
+        private int RolledDie;      // Default Value = 0
 
         public void StartGame()
+        {
+            PrintPlayerPosition();
+            RollADie();
+            PrintPlayerPosition();
+        }
+
+        private void RollADie()
+        {
+            Random generateNum = new Random();
+            RolledDie = generateNum.Next(7);
+            PlayerPosition += RolledDie; 
+            Console.WriteLine("Rolled Die: {0}", RolledDie);
+        }
+
+        private void PrintPlayerPosition()
         {
             Console.WriteLine("Player Position: {0}", PlayerPosition);
         }
